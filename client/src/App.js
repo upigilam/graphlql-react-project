be7985401c34f827df8f60745f46bbc127662d18
upendra pigilam <upigilam@paypal.com>
2018-12-29 22:06:13 -0800

    apollo set up in the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'; // binding apollo to react
 
 // components imports
-import BookList from './components/BookList'
+import BookList from './components/BookList';
+
+// apollo client setup
+const client = new ApolloClient({
+    // now apollo knows that we are making request to this endpoint from our app.
+    uri: 'http://localhost:4000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-          <h1>Reading list</h1>
-          <BookList/>
-      </div>
+        <ApolloProvider client={client}>
+          <div id="main">
+              <h1>Reading list</h1>
+              <BookList/>
+          </div>
+        </ApolloProvider>
     );
   }
 }
