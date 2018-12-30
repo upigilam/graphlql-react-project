f1d0740afbb4c565d54f3f784a8abed6d7db5b27
upendra pigilam <upigilam@paypal.com>
2018-12-29 21:41:41 -0800

    created new component for booklist and embedded in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
+// components imports
+import BookList from './components/BookList'
+
 class App extends Component {
   render() {
     return (
       <div id="main">
-              <h1>Reading list</h1>
-
+          <h1>Reading list</h1>
+          <BookList/>
       </div>
     );
   }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,19 @@
+/**
+ * Created by upigilam on 12/29/18.
+ */
+import React, { Component } from 'react';
+
+
+class BookList extends Component {
+    render() {
+        return (
+            <div>
+                <ul id="book-list">
+                    <li>Book name</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default BookList;
