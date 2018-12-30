91b4bee85fd62df061061e9c55679289027184b4
upendra pigilam <upigilam@paypal.com>
2018-12-30 00:48:48 -0800

    access the books list from the props and populate the list and show it on front end

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -15,12 +15,27 @@ const getBooksQuery = gql`
 `
 
 class BookList extends Component {
+    displayBooks() {
+        var data = this.props.data;
+        if (data.loading) {
+            return (<div>Loading books...</div>);
+        } else {
+            // iterate thru books and return the list(<li>)
+            // provide `key` attribute inside <li> tag to fix the below error:
+            // Warning: Each child in an array or iterator should have a unique "key" prop
+            return data.books.map(book => {
+                return (
+                    <li key={book.id}>{book.name}</li>
+                );
+            })
+        }
+    }
     render() {
-        console.log('props:: ', this.props);
+        // console.log('props:: ', this.props);
         return (
             <div>
                 <ul id="book-list">
-                    <li>Book name</li>
+                    {this.displayBooks()}
                 </ul>
             </div>
         );
