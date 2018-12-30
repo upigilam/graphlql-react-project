0ba1586075aba2a0d7338ec5cbdbbd7d94daad4a
upendra pigilam <upigilam@paypal.com>
2018-12-30 01:39:53 -0800

    creating prop states and attaching actions to an event and console log the events

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -3,33 +3,51 @@ import { graphql } from 'react-apollo'
 import { getAuthorsQuery } from '../queries/queries'
 
 class AddBook extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            genre: '',
+            authorId: ''
+        };
+    }
+
     displayAuthors() {
         var data = this.props.data;
         if (data.loading) {
-            return (<option>Loading Authors...</option>);
+            return (
+                <option> Loading Authors... </option>
+            );
         } else {
             return data.authors.map(author => {
                 return (
-                    <option key={author.id} value={author.id}>{author.name}</option>
+                    <option key = {author.id} value ={author.id}>{author.name}</option>
                 );
             })
         }
     }
+
+    submitForm(e) {
+        // preventing the default action on the event.
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         // console.log('props:: ', this.props);
         return (
-            <form id="add-book">
+            <form id="add-book" onSubmit={this.submitForm.bind(this)}>
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text"/>
+                    <input type="text" onChange={ (e) => this.setState({ name: e.target.value }) }/>
                 </div>
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text"/>
+                    <input type="text" onChange={ (e) => this.setState({ genre: e.target.value }) }/>
                 </div>
                 <div className="field">
                     <label>Author:</label>
-                    <select>
+                    <select onChange={ (e) => this.setState({ authorId: e.target.value }) }>
                         <option> select author</option>
                         {this.displayAuthors()}
                     </select>
